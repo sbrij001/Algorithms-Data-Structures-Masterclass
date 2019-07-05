33894796804d7fa937134beb06d83e9288e78efe
sbrij001 <sbrij001@yahoo.com>
2019-07-05 14:28:36 -0400

    sumZero time complexity O(n^2)

---
+++ problem-solving-patterns/sumZero.js
@@ -0,0 +1,26 @@
+//Multiple Pointers
+// creating pointers or values that correspond to an index or position
+// and move towards the begininng, end, or middle based on a certain condition
+
+
+//write a function called sumZero accepts a sorted array of integers
+// the function should find the first pair where the sum is 0
+// return an array that includes both values that sum to zero
+// or undefined if the pair does not exist.
+//sumZero([-3,-2,-1,0,1,2,3]) => [-3,3]
+//sumZero([1,2,3]) => undefined
+
+
+// NAIVE SOLUTION
+// Time complexity o(n^2)
+//space complexity - o(1)
+
+function sumZero(arr){
+  for (let i = 0; i < arr.length; i++) {
+    for (var j = i + 1; i < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i],arr[j]]
+      }
+    }
+  }
+}
