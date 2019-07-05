597dc866cc774524470d4b9bc59547de259115d8
sbrij001 <sbrij001@yahoo.com>
2019-07-05 14:36:50 -0400

    sumZero time complexity o(N)

--- problem-solving-patterns/sumZero.js
+++ problem-solving-patterns/sumZero.js
@@ -14,13 +14,30 @@
 // NAIVE SOLUTION
 // Time complexity o(n^2)
 //space complexity - o(1)
-
 function sumZero(arr){
   for (let i = 0; i < arr.length; i++) {
     for (var j = i + 1; i < arr.length; j++) {
       if (arr[i] + arr[j] === 0) {
         return [arr[i],arr[j]]
       }
+    }
+  }
+}
+
+//REFACTORED SOLUTION
+//time complexity o(N)
+//space complexity o(1)
+function sumZero(arr){
+  let left = 0
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right]
+    if (sum === 0) {
+      return [arr[left],arr[right]]
+    }else if (sum > 0) {
+      right--;
+    }else {
+      left ++
     }
   }
 }
