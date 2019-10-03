c78e0a3603ad05d8f5d8f65ae8e7cc63ae4ed04b
sbrij001 <sbrij001@yahoo.com>
2019-10-03 17:48:03 -0400

    created merge function that concats the 2 input arrays into one

--- problem-solving-patterns/merge-sort.js
+++ problem-solving-patterns/merge-sort.js
@@ -1,10 +1,28 @@
 // trial solution 1
-function mergeTwoArrays(arr1, arr2){
-  const result = [];
+// function mergeTwoArrays(arr1, arr2){
+//   const result = [];
+//
+//   result.push(arr1);
+//   const newArr = result.push(arr2);
+//   const flattend = newArr.flat()
+//   const sort = flattend.sort()
+//   return sort
+// }
 
-  result.push(arr1);
-  const newArr = result.push(arr2);
-  const flattend = newArr.flat()
-  const sort = flattend.sort()
-  return sort 
+// solution 2
+// follows the fundamentals of merge sort
+function mergeTwoArrays(arr1, arr2){
+  //base case
+  if( arr1.concat(arr2).length === 1){
+    return arr1.concat(arr2)
+  }
+
+  const left = arr1.sort();
+  const right = arr2.sort();
+  const merged = left.concat(right);
+  console.log(merged)
 }
+
+// function merge(arr){
+//
+// }
