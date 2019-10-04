13cff5a4e30cd71929f15b2bec3f46c0ad85b623
sbrij001 <sbrij001@yahoo.com>
2019-10-04 19:16:11 -0400

    created control flow for if an element from first arr is smaller than element from second arr

--- problem-solving-patterns/merge-sort.js
+++ problem-solving-patterns/merge-sort.js
@@ -1,7 +1,7 @@
 // solve for base cases if on of the arrays is empty return other array
 // create a merged arr that we will return
-// loop and check if the number is true
-// create two variable that will store the first element in arr for both arrs
+//create two variable that will store the first element in arr for both arrs
+//loop and check if the number is true
 // loop thru the arrays
 // check if first arr element is less than second array element
 // if so push that ele into merged arr
@@ -20,6 +20,22 @@ function merge = (nums1, m, nums2,n) => {
   }
   if(n === 0){
       return nums1
+  }
+
+
+[1,2,3] [2,5,6]
+firstEleInNums1 = 1
+firstEleInNums1 = 2
+  while(firstEleInNums1 || firstEleInNums2){
+    if(!firstEleInNums1 || firstEleInNums1 < firstEleInNums2 ){
+      merged.push(firstEleInNums1)
+      firstEleInNums1 = arr[i]
+      i++
+    }else {
+      merge.push(firstEleInNums2)
+      firstEleInNums2 = arr[j]
+      j++
+    }
   }
 
 }
