01c25cdce922e3c5fa1e57e02f374691206f3f2f
sbrij001 <sbrij001@yahoo.com>
2019-10-04 20:03:40 -0400

    created pseudocode with loic for merge helper function

--- problem-solving-patterns/merge-sort.js
+++ problem-solving-patterns/merge-sort.js
@@ -1,2 +1,18 @@
 // assume 2 arrays are sorted
 // returned a merged sorted array
+// what do we do if the the 2 arrays are not the same size?
+
+// Merging arrays pseudocode
+
+// create an empty array
+// take a look at the smallest values in each input array
+  // use a while loop
+  // need 2 counters one called i and one called j
+  // j and i start at 0
+// while there are 2 arrays
+  // take the item of the first array of the first item
+  // check of that value is smaller than the item of the second array of the 1 item
+  // push the item that is smaller in the result arr
+  // once we finish with all the values in the first array
+  // we push the rest of the items from the second array
+  
